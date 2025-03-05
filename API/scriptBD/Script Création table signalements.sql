af3e1caa94302da10d3276cb9f87524b054a7fa6
Adrien ASTIER <adrien.astier@iut-rodez.fr>
2025-03-05 09:19:44 +0100

    Modif script bd

--- API/scriptBD/Script Création table signalements.sql
+++ API/scriptBD/Script Création table signalements.sql
@@ -8,5 +8,9 @@ CREATE TABLE IF NOT EXISTS signalements (
 	titre VARCHAR(50) NOT NULL,
 	resume VARCHAR(200) NOT NULL,
 	impact INT(1) NOT NULL,
-	recontact BOOLEAN NOT NULL
+	recontact BOOLEAN NOT NULL,
+	id_employe CHAR(7) NOT NULL
 ) CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+ALTER TABLE `signalements`
+  ADD CONSTRAINT `fk_employe` FOREIGN KEY (`id_employe`) REFERENCES `login` (`id_employe`);
