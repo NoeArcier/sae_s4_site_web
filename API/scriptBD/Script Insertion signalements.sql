fd09013b0660ccc360eaa2456d61b55a6858bb00
Adrien ASTIER <adrien.astier@iut-rodez.fr>
2025-03-05 10:22:26 +0100

    Modif encore

--- API/scriptBD/Script Insertion signalements.sql
+++ API/scriptBD/Script Insertion signalements.sql
@@ -1,4 +1,4 @@
 -- Insertion des données dans la table employe
-INSERT INTO signalements (date, heure, titre, resume, impact, recontact, id_employe) VALUES
+INSERT INTO signalements (date, heure, titre, resume, impact, recontact, id_reservation) VALUES
 (CURRENT_DATE(), CURRENT_TIME(), 'Ceci est une très beau test', 'Ceci est un très beau test de resume', 2, TRUE, 'R000001'),
 (CURRENT_DATE(), CURRENT_TIME(), 'Deuxième titre', 'Deuxième résume', 0, FALSE, 'R000011');
