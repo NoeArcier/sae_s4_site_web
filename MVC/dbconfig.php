cae26e552020cb9646fe8c98e80558f4a22769b9
NoixDeXydre <noixdexydre@mailfence.com>
2025-03-26 09:40:05 +0100

    Suppression du dossier MVC pour le renommer plus tard

--- MVC/dbconfig.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'db_host' => 'sae_s4_mvc_db',
-    'db_port' => 3306,
-    'db_name' => 'StatisalleBD',
-    'db_user' => 'user',
-    'db_pass' => 'mdp',
-    'db_charset' => 'utf8mb4'
-];
