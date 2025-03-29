893c0afe7f9503d922f38b09cb010eb2ae507869
NoixDeXydre <noixdexydre@mailfence.com>
2025-03-29 12:56:30 +0100

    Suppression de l'utilisation de Docker

--- mvc/dbconfig.php
+++ mvc/dbconfig.php
@@ -1,9 +1,10 @@
 <?php
-return [
-    'db_host' => 'localhost', // Docker : 'sae_s4_mvc_db'
-    'db_port' => 3306,
-    'db_name' => 'StatisalleBD',
-    'db_user' => 'application', // Docker : 'user'
-    'db_pass' => '@ppl1cat1on123', // Docker : 'pass'
-    'db_charset' => 'utf8mb4'
-];
+    return [
+        'db_host' => 'localhost',
+        'db_port' => 3306,
+        'db_name' => 'StatisalleBD',
+        'db_user' => 'application',
+        'db_pass' => '@ppl1cat1on123',
+        'db_charset' => 'utf8mb4'
+    ];
+?>
